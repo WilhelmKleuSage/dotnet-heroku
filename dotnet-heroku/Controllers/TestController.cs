c52d71ed8b00e463ecde169ecbca9936d5b8d8dc
Wilhelm Kleu <wilhelm.kleu@sage.com>
2017-08-18 12:54:46 +0200

    Basic controller

---
+++ dotnet-heroku/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_heroku.Controllers
+{
+    [Route("/")]
+    public class TestController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Running";
+        }
+
+    }
+}
