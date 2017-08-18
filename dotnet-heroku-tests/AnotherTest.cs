ddae6d493828efb83fedb2a7c3b8c11a7ea798ef
Wilhelm Kleu <wilhelm.kleu@sage.com>
2017-08-18 14:54:50 +0200

    Added another test

---
+++ dotnet-heroku-tests/AnotherTest.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace dotnet_heroku_tests
+{
+    public class AnotherTest
+    {
+        [Fact]
+        public void ShouldPass()
+        {
+            true.Should().BeTrue();
+        }
+    }
+}
