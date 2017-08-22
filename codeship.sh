d41ba752a5281203c0d0f5b6863ba3c6644a787f
Wilhelm Kleu <wilhelm.kleu@sage.com>
2017-08-22 16:18:59 +0200

    fix tar location

--- codeship.sh
+++ codeship.sh
@@ -6,7 +6,7 @@ set -e
 
 mkdir $HOME/dotnet/dotnet
 curl -o dotnet.tar.gz -sSL https://download.microsoft.com/download/1/B/4/1B4DE605-8378-47A5-B01B-2C79D6C55519/dotnet-sdk-2.0.0-linux-x64.tar.gz
-tar -xvf dotnet.tar.gz -C $HOME/dotnet/dotnet
+tar -xvf dotnet.tar.gz -C $HOME/dotnet
 ln -s $HOME/dotnet/dotnet $HOME/bin/dotnet
 
 dotnet --version
