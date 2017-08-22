d0762cd4ae724ec75f6ec9e4f9ac56460b52f62c
Wilhelm Kleu <wilhelm.kleu@sage.com>
2017-08-22 15:55:37 +0200

    fix curl in codeship script

--- codeship.sh
+++ codeship.sh
@@ -6,7 +6,7 @@ set -e
 
 mkdir dotnet
 cd dotnet
-curl -o -sSL dotnet.tar.gz curl https://download.microsoft.com/download/1/B/4/1B4DE605-8378-47A5-B01B-2C79D6C55519/dotnet-sdk-2.0.0-linux-x64.tar.gz
+curl -o -sSL dotnet.tar.gz https://download.microsoft.com/download/1/B/4/1B4DE605-8378-47A5-B01B-2C79D6C55519/dotnet-sdk-2.0.0-linux-x64.tar.gz
 tar -xvf dotnet.tar.gz
 
 ./dotnet-install.sh --install-dir $HOME/dotnet
