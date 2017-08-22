0ddc162cb1933a8bbec647deb0ea292a6fb4819d
Wilhelm Kleu <wilhelm.kleu@sage.com>
2017-08-22 16:48:12 +0200

    Add another test project to test that codeship runs all test projects

---
+++ AnotherProject.Tests/YetAnotherTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace AnotherProject.Tests
+{
+    public class YetAnotherTest
+    {
+        [Fact]
+        public void TestTrueIsTrue()
+        {
+            Assert.True(true);
+        }
+    }
+}
