15b3d89216f975f436ec635eb59e352aa4519577
Wilhelm Kleu <wilhelm.kleu@sage.com>
2017-08-22 16:12:26 +0200

    remove running of dotnet-install script

--- codeship.sh
+++ codeship.sh
@@ -8,8 +8,7 @@ mkdir dotnet
 cd dotnet
 curl -o dotnet.tar.gz -sSL https://download.microsoft.com/download/1/B/4/1B4DE605-8378-47A5-B01B-2C79D6C55519/dotnet-sdk-2.0.0-linux-x64.tar.gz
 tar -xvf dotnet.tar.gz
-
-./dotnet-install.sh --install-dir $HOME/dotnet
 ln -s $HOME/dotnet/dotnet $HOME/bin/dotnet
+cd..
 
 dotnet --version
