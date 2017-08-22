439a88ccc58221075b90e8857fd15113389e333e
Wilhelm Kleu <wilhelm.kleu@sage.com>
2017-08-22 16:17:21 +0200

    reference directories relative to $HOME

--- codeship.sh
+++ codeship.sh
@@ -4,11 +4,9 @@
 
 set -e
 
-mkdir dotnet
-cd dotnet
+mkdir $HOME/dotnet/dotnet
 curl -o dotnet.tar.gz -sSL https://download.microsoft.com/download/1/B/4/1B4DE605-8378-47A5-B01B-2C79D6C55519/dotnet-sdk-2.0.0-linux-x64.tar.gz
-tar -xvf dotnet.tar.gz
+tar -xvf dotnet.tar.gz -C $HOME/dotnet/dotnet
 ln -s $HOME/dotnet/dotnet $HOME/bin/dotnet
-cd..
 
 dotnet --version
