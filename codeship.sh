4bbca78fd0c4a983f2fbc42abd77943a66c1d57c
Wilhelm Kleu <wilhelm.kleu@sage.com>
2017-08-30 11:55:17 +0200

    Echo if dotnet core being downloaded or used from cache

--- codeship.sh
+++ codeship.sh
@@ -4,11 +4,13 @@
 
 set -e
 
-if ! [ -d "$HOME/cache/dotnet" ]; then
-	echo "Downloading dotnet core"
-	mkdir $HOME/cache/dotnet
-	curl -o dotnet.tar.gz -sSL https://download.microsoft.com/download/1/B/4/1B4DE605-8378-47A5-B01B-2C79D6C55519/dotnet-sdk-2.0.0-linux-x64.tar.gz
-	tar -xvf dotnet.tar.gz -C $HOME/cache/dotnet
+if [ -d "$HOME/cache/dotnet" ]; then
+  echo "dotnet found in cache"
+else
+  echo "Downloading dotnet core"
+  mkdir $HOME/cache/dotnet
+  curl -o dotnet.tar.gz -sSL https://download.microsoft.com/download/1/B/4/1B4DE605-8378-47A5-B01B-2C79D6C55519/dotnet-sdk-2.0.0-linux-x64.tar.gz
+  tar -xvf dotnet.tar.gz -C $HOME/cache/dotnet
 fi
 
 ln -s $HOME/cache/dotnet/dotnet $HOME/bin/dotnet
